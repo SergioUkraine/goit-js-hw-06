4a8acee7142bdc2b5c43db3fdd5454860c76cb3c
Serhii <38428786+SergioUkraine@users.noreply.github.com>
2023-04-08 15:14:36 +0300

    tast7 done

---
+++ js/task-07.js
@@ -0,0 +1,12 @@
+/*
+Напиши скрипт, який реагує на зміну значення input#font-size-control (подія input) і змінює інлайн-стиль span#text, 
+оновлюючи властивість font-size. В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
+*/
+
+
+const inputSlider = document.querySelector('#font-size-control');
+const spanText = document.querySelector('#text');
+
+inputSlider.addEventListener('input', (event) => {
+    spanText.style.fontSize = `${event.currentTarget.value}px`;
+});
